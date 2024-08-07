6a57e0d1fd20307ac8e90251c11cfbfa8963574b
ayeluvsu <158154505+ayeluvsu@users.noreply.github.com>
2024-08-07 21:59:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = [".gg/bisakol","Browsing Bisakol Service"];
+const statusMessages = [".gg/angelshop","Browsing Angel Shop"];
 
 
 let currentIndex = 0;
